1c7761f18c33a7b6d91528bfdf74520c09226aba
Jonas Vetsch <jonas@vetsch.com>
2025-06-15 23:07:04 +0200

    Fetch a question and display it in the UI.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchFirstQuestion } from '../services/questionService';
 import { signOut } from 'firebase/auth';
 import { auth } from '../services/firebase';
+import type { Question } from '../model/question';
 
 export default function MainPage() {
+  const [question, setQuestion] = useState<Question | null>(null);
+
   useEffect(() => {
     const load = async () => {
       const q = await fetchFirstQuestion();
-      console.log(q);
+      setQuestion(q);
     };
     load();
   }, []);
@@ -16,6 +19,21 @@ export default function MainPage() {
     <div>
       <h2>Hi, there.</h2>
       <button onClick={() => signOut(auth)}>Logout</button>
+
+      {question ? (
+        <div style={{ marginTop: '2rem' }}>
+          <h3>{question.questionText}</h3>
+          <ul>
+            {question.answers.map((answer, idx) => (
+              <li key={idx}>
+                <button>{answer}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading question...</p>
+      )}
     </div>
   );
 }
