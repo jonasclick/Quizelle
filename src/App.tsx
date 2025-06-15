f16759be9278bc5369899a96bc9265fc7b275120
Jonas Vetsch <jonas@vetsch.com>
2025-06-15 11:25:54 +0200

    Add Firebase and use Firestore to fetch questions from the DB.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './services/firebase';
+import type { Question } from './model/question.ts';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [question, setQuestion] = useState<Question | null>(null);
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      const querySnapshot = await getDocs(collection(db, 'questions'));
+      const docs = querySnapshot.docs.map((doc) => doc.data() as Question);
+      if (docs.length > 0) {
+        setQuestion(docs[0]); // Get the first question
+      }
+    };
+
+    fetchQuestion();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h3>{question ? question.questionText : 'Loading...'}</h3>
+      <ul>
+        {question?.answers.map((answer, index) => (
+          <li key={index}>
+            <button>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
