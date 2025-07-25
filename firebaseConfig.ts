2328fcd3886d9dd76ba62ea7df28dac109034023
Jonas Vetsch <jonas@vetsch.com>
2025-07-25 16:28:41 +0200

    Add firebaseConfig.ts to enable GitHub CI/CD

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBZjOQACq9e9evJ8nClwBIOq6yaNKH8BBk',
+  authDomain: 'jumpinapp.firebaseapp.com',
+  projectId: 'jumpinapp',
+  storageBucket: 'jumpinapp.firebasestorage.app',
+  messagingSenderId: '657728415546',
+  appId: '1:657728415546:web:432db8c9cfa88c689f5203',
+};
